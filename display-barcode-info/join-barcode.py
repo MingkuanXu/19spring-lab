5388b325cd3b49764f6330d109f945ecaeeeb107
Mingkuan Xu <mingkuan.xu@duke.edu>
2019-09-18 21:39:10 -0400

    Join barcode.

---
+++ display-barcode-info/join-barcode.py
@@ -0,0 +1,17 @@
+import sys
+
+
+f1 = open("sample-barcode.txt","r")
+f2 = open("barcode-output.txt","w+")
+
+line = f1.readline()
+line = f1.readline()
+while True:
+    newline = "".join(line.split("	"))
+    print(newline)
+    f2.write(newline)
+    line = f1.readline()
+    if not line:
+        break
+f1.close()
+f2.close()
